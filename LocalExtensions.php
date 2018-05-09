5955b2dd0e481c2bbfadd4ebc48af428c08965bb
paladox <paladox@users.noreply.github.com>
2018-05-09 23:41:16 +0100

    Fix extension name

--- LocalExtensions.php
+++ LocalExtensions.php
@@ -8,7 +8,7 @@ if ( $wmgUseAddThis ) {
 }
 
 if ( $wmgUseAddHTMLMetaAndTitle ) {
-	wfLoadExtension( 'UseAddHTMLMetaAndTitle' );
+	wfLoadExtension( 'AddHTMLMetaAndTitle' );
 }
 
 if ( $wmgUseAdminLinks ) {
